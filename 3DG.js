f1dbbc226d59d87c213328ac96cbe826af58bf69
Jonathan Zhang <78021210+Zzzz-Jonathan@users.noreply.github.com>
2021-11-04 18:27:47 +0800

    001

--- 3DG.js
+++ 3DG.js
@@ -1,7 +1,7 @@
-var mapSize = [10000,10000,10000]
-var scene = [], destination = [], camera = [];
+var mapSize = [10000,10000,10000], cameraSize = 1000;//
+var scene = [], destination = [], camera = [], renderer = [];
 
-function Env(scene,destination,camera){
+function Env(scene,destination,camera,renderer){
     this.init = function(){
         var scene_new = new THREE.Scene();
         scene.push(scene_new);
@@ -15,7 +15,30 @@ function Env(scene,destination,camera){
         var axesHelper = new THREE.AxesHelper(250);
         scene[0].add(axesHelper);
         //至此，创建了sence，destination和axeshelper
-        
+        var camera_new = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+        camera.push(camera_new);
+        this.cameraPosition = [0,0,0];
+        this.cameraDirection = [0,0,0];
+        this.moveCamera();
+        //设置相机
+        var renderer_new = new THREE.WebGLRenderer();
+        renderer.push(renderer_new);
+        this.reSizeMap();
+        //设置画布
+        var ambient = new THREE.AmbientLight(0xffffff);
+        scene[0].add(ambient);
+        //设置环境光
+    }
+    this.moveCamera = function (){
+
+        camera[0].position.set(this.cameraPosition);
+        camera[0].lookAt(new THREE.Vector3(this.cameraDirection[0],this.cameraDirection[1],this.cameraDirection[2]));
+    }
+    this.reSizeMap = function (){
+        var width = window.innerWidth, height = window.innerHeight; //窗口高度
+        renderer.setSize(width, height);//设置渲染区域尺寸
+        renderer.setClearColor(0xffffff, 1); //设置背景颜色
+        document.body.appendChild(renderer.domElement); //body元素中插入canvas对象
     }
     this.fresh = function(){
         destination[0].translateX(Math.random()*mapSize[0]);
