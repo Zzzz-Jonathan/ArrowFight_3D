0fe43196cc55689b07a7d23c76804a5d0ce621ec
Jonathan Zhang <78021210+Zzzz-Jonathan@users.noreply.github.com>
2021-12-07 12:09:50 +0800

    12 07 12:09

--- worker.js
+++ worker.js
@@ -1,7 +1,11 @@
 import {Communicate} from "./sendInf.js"
 
 var comm = new Communicate()
-
+var url = '', data = [undefined]
 var polling = function (){
+    comm.POSTJSON()
+}
 
+self.onmessage = function (event){
+    data[0] = event.data
 }
